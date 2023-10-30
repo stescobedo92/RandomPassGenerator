94a4c322db5c9ea77f08ba4351d808f6f731182e
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-10-30 16:26:32 -0600

    Moving logic to the Generator class

---
+++ RandomPassGenerator/Generator.cpp
@@ -0,0 +1,17 @@
+#include "Generator.h"
+
+std::string Generator::genPassword(const size_t length) {
+    const std::string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&&*()_+";
+    std::string password;
+
+    std::mt19937 generator(std::time(0));
+    std::uniform_int_distribution<int> distribution(0, characters.size() - 1);
+
+    for (std::size_t i = 0; i < length; ++i) {
+        std::size_t index = distribution(generator);
+
+        password += characters[index];
+    }
+
+    return password;
+}
---
+++ RandomPassGenerator/Generator.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <random>
+#include <string>
+#include <string_view>
+#include <ctime>
+
+class Generator
+{
+public:
+	std::string genPassword(const size_t length);
+};
+
